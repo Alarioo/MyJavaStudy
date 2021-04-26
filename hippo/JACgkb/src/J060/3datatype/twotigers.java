76fce08e7ec26868429a1f1054de6a57962ae60c
alario <realmewyyx@163.com>
2021-04-26 14:21:56 +0800

    recommit

--- hippo/JACgkb/src/J060/3datatype/twotigers.java
+++
@@ -1,9 +0,0 @@
-//判断两只老虎体重是否相等
-public class twotigers{
-    public static void main(String[] args){
-    int tiger1=100;
-    int tiger2=200;
-    boolean r=tiger1==tiger2? true: false;
-    System.out.println(r);
-    }
-}
--- hippo/JACgkb/src/day61_83/test.java
+++
@@ -1,7 +0,0 @@
-package day61_83;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("d过得更舒服");
-    }
-}
