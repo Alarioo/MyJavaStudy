427c7ad94c12f1773ae3ed276146a78bc07b2a83
alario <realmewyyx@163.com>
2021-04-30 21:53:38 +0800

    0430

--- hippo/JACutf8/src/day221_250/priorityqueue/MedianFinder.java
+++ hippo/JACutf8/src/day221_250/priorityqueue/MedianFinder.java
@@ -1,2 +1,41 @@
-package day221_250.priorityqueue;public class MedianFinder {
+package day221_250.priorityqueue;
+
+import java.util.PriorityQueue;
+
+public class MedianFinder {
+    /* 大顶堆，存储左半边元素 */
+    private PriorityQueue<Integer> left = new PriorityQueue<>((o1, o2) -> o2 - o1);  //lamda表达式
+    /*
+         PriorityQueue<>((o1, o2) -> o2 - o1);  //lamda表达式
+		Queue<Integer> q=new PriorityQueue<Integer>(new Comparator<Integer>(){
+			public int compare(Integer o1, Integer o2) {
+				return o2-o1;//若是o1-o2,则为小顶堆
+			}
+     */
+    /* 小顶堆，存储右半边元素，并且右半边元素都大于左半边 */
+    private PriorityQueue<Integer> right = new PriorityQueue<>();
+    /* 当前数据流读入的元素个数 */
+    private int N = 0;
+
+    public void Insert(Integer val) {
+        /* 插入要保证两个堆存于平衡状态 */
+        if (N % 2 == 0) {
+            /* N 为偶数的情况下插入到右半边。
+             * 因为右半边元素都要大于左半边，但是新插入的元素不一定比左半边元素来的大，
+             * 因此需要先将元素插入左半边，然后利用左半边为大顶堆的特点，取出堆顶元素即为最大元素，此时插入右半边 */
+            left.add(val);
+            right.add(left.poll());
+        } else {
+            right.add(val);
+            left.add(right.poll());
+        }
+        N++;
+    }
+
+    public Double GetMedian() {
+        if (N % 2 == 0)
+            return (left.peek() + right.peek()) / 2.0;
+        else
+            return (double) right.peek();
+    }
 }
--- hippo/JACutf8/src/day221_250/priorityqueue/TEST.java
+++ hippo/JACutf8/src/day221_250/priorityqueue/TEST.java
@@ -1,2 +1,32 @@
-package day221_250.priorityqueue;public class TEST {
+package day221_250.priorityqueue;
+
+import java.util.PriorityQueue;
+
+public class TEST {
+    /** initialize your data structure here. */
+    private PriorityQueue<Integer> left=new PriorityQueue<>();
+    private PriorityQueue<Integer> right=new PriorityQueue<>((o1, o2) -> o2 - o1);
+    private int N;
+    public MedianFinder() {
+
+    }
+
+    public void addNum(int num) {
+        if(N%2==0){
+            left.add(num);
+            right.add(left.poll());
+        }else{
+            right.add(num);
+            left.add(right.poll());
+        }
+        N++;
+    }
+
+    public double findMedian() {
+        if(N%2==0){
+            return (left.peek()+right.peek())/2.0;
+        }else{
+            return right.peek();
+        }
+    }
 }
